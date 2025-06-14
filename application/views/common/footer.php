004997a5a9f23c5079e2bfe9fa151527563cacc2
deepeshyadavrd <raoshsbh6@gmail.com>
2025-06-14 17:35:00 +0530

    added footer file

---
+++ application/views/common/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3 bg-dark text-white">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> Urbanwood Business Directory. All rights reserved.</p>
+    </div>
+</footer>
